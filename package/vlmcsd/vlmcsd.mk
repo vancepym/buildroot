989311530ca0e599bd7bd9aad6e9a7c9dd1d62b4
Scott Fan <fancp2007@gmail.com>
2015-07-31 14:35:09 +0800

    vlmcsd: new package
    Signed-off-by: Scott Fan <fancp2007@gmail.com>

---
+++ package/vlmcsd/vlmcsd.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# vlmcsd
+#
+################################################################################
+
+VLMCSD_VERSION = 0c0b23a496ab9d556672fe61967fc90f52ed3464
+VLMCSD_SITE = $(call github,vancepym,vlmcsd,$(VLMCSD_VERSION))
+
+define VLMCSD_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+endef
+
+define VLMCSD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/vlmcsd \
+		$(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/vlmcs \
+		$(TARGET_DIR)/usr/sbin
+endef
+
+define VLMCSD_INSTALL_INIT_SYSV
+	$(INSTALL) -m 755 -D package/vlmcsd/S49vlmcsd \
+		$(TARGET_DIR)/etc/init.d/S49vlmcsd
+endef
+
+$(eval $(generic-package))
